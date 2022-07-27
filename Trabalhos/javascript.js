03bef2a72d1484c3fcf83486fb20f78d348dc16b
Felipe Muure <Felipe_Muure@outlook.com>
2022-07-27 05:13:49 -0300

    1.0

--- Trabalhos/javascript.js
+++ Trabalhos/javascript.js
@@ -1,9 +1,11 @@
+console.log(windows.location.pathname);
+
 function Carregar() {
     var data = new Date()
     var dia = data.getDay()
     var hora = data.getHours()
     var res = document.getElementById('res')
-    var url = window.location.href;
+    var int = document.getElementById('int')
     var segunda = document.getElementById('segunda')
     var terca = document.getElementById('terca')
     var quarta = document.getElementById('quarta')
@@ -11,18 +13,28 @@ function Carregar() {
     var sexta = document.getElementById('sexta')
     var sabado = document.getElementById('sabado')
     var fundo = document.getElementById('fundo')
+    var link = window.location.pathname;
+    var bar = document.getElementById('bar')
     
-    /*Mensagem topo do Header*/
-    if (hora < 6 || hora > 22) {
-        res.innerHTML = 'A acadêmia ainda não abriu, meu querido!'
-    } else if (hora < 12) {
-        res.innerHTML = 'Bom dia!!! Hoje você me parece bem animado!'
-    } else if (hora < 19) {
-        res.innerHTML = 'Boa Tarde! É sempre bom cuidar da saúde!'
+    /*Movimentação da barra abaixo do menu*/
+    if (link == "/Trabalhos/Segunda.html") {
+        bar.style.left = "1.8%";
+    } else if (link == "/Trabalhos/Terca.html") {
+        bar.style.left = "18%";
+    } else if (link == "/Trabalhos/Quarta.html") {
+        bar.style.left = "34.4%";
+    } else if (link == "/Trabalhos/Quinta.html") {
+        bar.style.left = "50.6%";
+    } else if (link == "/Trabalhos/Sexta.html") {
+        bar.style.left = "66.9%";
+    } else if (link == "/Trabalhos/Sabado.html") {
+        bar.style.left = "83.2%";
     } else {
-        res.innerHTML = 'Boa Noite!!! Academia lotadinha!!!"'
+
     }
-    res.innerHTML = url
+
+ 
+    
 
     /*Verifica o dia da semana e o destaca*/
     if (dia == 1) {
@@ -43,10 +55,11 @@ function Carregar() {
     } else if (dia == 3) {
         fundo.style.backgroundImage = "url('https://media.istockphoto.com/photos/fitness-or-bodybuilding-background-dumbbells-on-gym-floor-top-view-picture-id908224814?k=20&m=908224814&s=170667a&w=0&h=0eBoO783ox1x5qOt_4wGPcnj2ugvhwtzNDKcO3tGbBo=')";
         fundo.style.backgroundSize = "100%";
-        res.innerHTML = 'Quarta-Feira da alegria.<br>  Parece que o treino ta indo. Continue assim!'
+        res.innerHTML = 'Quarta-Feira da alegria!!<br>  Parece que o treino ta indo. Continue assim!'
         quarta.style.fontSize = "15pt";
         quarta.style.fontWeight = "bold";
         quarta.style.color = "#A2C640";
+        
     } else if (dia == 4) {
         fundo.style.backgroundImage = "url('https://media.istockphoto.com/photos/gym-background-with-fitness-equipment-concept-workout-from-home-stay-picture-id1221818374?k=20&m=1221818374&s=170667a&w=0&h=OVURTGo6Vea6uqVSWk5VNIylBD4ctRpK9NJrG1Jr_SA=')";
         fundo.style.backgroundPositionY = "100%";
@@ -79,6 +92,8 @@ function Carregar() {
     }
 }
 
+
+/*Click para abrir e ampliar os exercicios do 0 ao 20*/
 function Zoom0() {
     var click = document.getElementById('Zoom0')
     var img = document.getElementById('imgZoom0')
@@ -481,3 +496,4 @@ function Zoom20() {
     }
 
 }
+
