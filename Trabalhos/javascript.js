fa72eed2fb4d68ffe7a51285ef0c7e6bf65886f4
Felipe Muure <Felipe_Muure@outlook.com>
2022-08-03 16:28:06 -0300

    v0.01

--- Trabalhos/javascript.js
+++ Trabalhos/javascript.js
@@ -238,19 +238,22 @@ function Zoom0() {
     var img = document.getElementById('imgZoom0')
     var aside = document.getElementById('asideZoom0')
     var esteira = document.getElementById('esteira')
+    var hide = document.getElementById('hide0')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
         esteira.innerHTML = 'Bike ou Esteira 20 min';
         
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
         esteira.innerHTML = "<ul>Andar ou correr na Esteira/Bicicleta serve:<br><br></ul><li>Tonifica os músculos</li><li>Ajuda a diminuir a pressão arterial</li><li>Melhora a circulação sanguínea</li><li>Amplia a capacidade pulmonar</li>";
     }
 
@@ -261,17 +264,20 @@ function Zoom1() {
     var click = document.getElementById('Zoom1')
     var img = document.getElementById('imgZoom1')
     var aside = document.getElementById('asideZoom1')
+    var hide = document.getElementById('hide1')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -280,17 +286,20 @@ function Zoom2() {
     var click = document.getElementById('Zoom2')
     var img = document.getElementById('imgZoom2')
     var aside = document.getElementById('asideZoom2')
+    var hide = document.getElementById('hide2')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -299,17 +308,20 @@ function Zoom3() {
     var click = document.getElementById('Zoom3')
     var img = document.getElementById('imgZoom3')
     var aside = document.getElementById('asideZoom3')
+    var hide = document.getElementById('hide3')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -318,17 +330,20 @@ function Zoom4() {
     var click = document.getElementById('Zoom4')
     var img = document.getElementById('imgZoom4')
     var aside = document.getElementById('asideZoom4')
+    var hide = document.getElementById('hide4')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -337,17 +352,20 @@ function Zoom5() {
     var click = document.getElementById('Zoom5')
     var img = document.getElementById('imgZoom5')
     var aside = document.getElementById('asideZoom5')
+    var hide = document.getElementById('hide5')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -356,17 +374,20 @@ function Zoom6() {
     var click = document.getElementById('Zoom6')
     var img = document.getElementById('imgZoom6')
     var aside = document.getElementById('asideZoom6')
+    var hide = document.getElementById('hide6')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -375,17 +396,20 @@ function Zoom7() {
     var click = document.getElementById('Zoom7')
     var img = document.getElementById('imgZoom7')
     var aside = document.getElementById('asideZoom7')
+    var hide = document.getElementById('hide7')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -394,17 +418,20 @@ function Zoom8() {
     var click = document.getElementById('Zoom8')
     var img = document.getElementById('imgZoom8')
     var aside = document.getElementById('asideZoom8')
+    var hide = document.getElementById('hide8')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -413,17 +440,20 @@ function Zoom9() {
     var click = document.getElementById('Zoom9')
     var img = document.getElementById('imgZoom9')
     var aside = document.getElementById('asideZoom9')
+    var hide = document.getElementById('hide9')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -432,17 +462,20 @@ function Zoom10() {
     var click = document.getElementById('Zoom10')
     var img = document.getElementById('imgZoom10')
     var aside = document.getElementById('asideZoom10')
+    var hide = document.getElementById('hide10')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -451,17 +484,20 @@ function Zoom11() {
     var click = document.getElementById('Zoom11')
     var img = document.getElementById('imgZoom11')
     var aside = document.getElementById('asideZoom11')
+    var hide = document.getElementById('hide11')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -470,17 +506,20 @@ function Zoom12() {
     var click = document.getElementById('Zoom12')
     var img = document.getElementById('imgZoom12')
     var aside = document.getElementById('asideZoom12')
+    var hide = document.getElementById('hide12')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -489,17 +528,20 @@ function Zoom13() {
     var click = document.getElementById('Zoom13')
     var img = document.getElementById('imgZoom13')
     var aside = document.getElementById('asideZoom13')
+    var hide = document.getElementById('hide13')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -508,17 +550,20 @@ function Zoom14() {
     var click = document.getElementById('Zoom14')
     var img = document.getElementById('imgZoom14')
     var aside = document.getElementById('asideZoom14')
+    var hide = document.getElementById('hide14')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -527,17 +572,20 @@ function Zoom15() {
     var click = document.getElementById('Zoom15')
     var img = document.getElementById('imgZoom15')
     var aside = document.getElementById('asideZoom15')
+    var hide = document.getElementById('hide15')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -546,17 +594,20 @@ function Zoom16() {
     var click = document.getElementById('Zoom16')
     var img = document.getElementById('imgZoom16')
     var aside = document.getElementById('asideZoom16')
+    var hide = document.getElementById('hide16')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -565,17 +616,20 @@ function Zoom17() {
     var click = document.getElementById('Zoom17')
     var img = document.getElementById('imgZoom17')
     var aside = document.getElementById('asideZoom17')
+    var hide = document.getElementById('hide17')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -584,17 +638,20 @@ function Zoom18() {
     var click = document.getElementById('Zoom18')
     var img = document.getElementById('imgZoom18')
     var aside = document.getElementById('asideZoom18')
+    var hide = document.getElementById('hide18')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -603,17 +660,20 @@ function Zoom19() {
     var click = document.getElementById('Zoom19')
     var img = document.getElementById('imgZoom19')
     var aside = document.getElementById('asideZoom19')
+    var hide = document.getElementById('hide19')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
@@ -622,17 +682,20 @@ function Zoom20() {
     var click = document.getElementById('Zoom20')
     var img = document.getElementById('imgZoom20')
     var aside = document.getElementById('asideZoom20')
+    var hide = document.getElementById('hide20')
 
-    if(click.style.height === "600px") {
-        click.style.height = "110px"
+    if(click.style.height === "635px") {
+        click.style.height = "130px"
         click.setAttribute('class', 'Zoom2')
         img.setAttribute('class', 'imgZoom2')
         aside.setAttribute('class', 'asideZoom2')
+        hide.style.marginLeft = '-265px';
     } else {
-        click.style.height = "600px"
+        click.style.height = "635px"
         click.setAttribute('class', 'Zoom')
         img.setAttribute('class', 'imgZoom')
         aside.setAttribute('class', 'asideZoom')
+        hide.style.marginLeft = '0px';
     }
 
 }
